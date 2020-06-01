7a0e260f5c90d138d16798a3529a9ceb46bdffa9
Sayuj Sehgal <sayujsehgal@iprakash.com>
2020-06-01 13:39:13 +0530

    design ready for online rooms

--- client/src/components/join/Join.js
+++ client/src/components/join/Join.js
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import ChatContext from '../../context/chat/chatContext';
 import { ENV_ENDPOINT } from '../../config/default';
 import io from 'socket.io-client';
+import OnlineRooms from '../onlineRooms/OnlineRooms';
 
 import Styles from './Join.module.css';
 
@@ -66,37 +67,46 @@ const Join = ({ location }) => {
   };
 
   return (
-    <div className={Styles.outerContainer}>
-      <div className={Styles.innerContainer}>
-        <h1 className={Styles.heading}>Welcome To Chatme</h1>
-        <div className={Styles.fieldContainer}>
-          <div className={Styles.inputField}>
-            <label htmlFor='name'>Name</label>
-            <input
-              type='text'
-              name='name'
-              id='name'
-              placeholder='Enter your name'
-              value={userInfo.name}
-              onChange={e => handleChange(e)}
-              required
-            />
+    <div className={Styles.container}>
+      <div className={Styles.chatFormContainer}>
+        <div className={Styles.outerContainer}>
+          <div className={Styles.innerContainer}>
+            <h1 className={Styles.heading}>Welcome To Chatme</h1>
+            <div className={Styles.fieldContainer}>
+              <div className={Styles.inputField}>
+                <label htmlFor='name'>Name</label>
+                <input
+                  type='text'
+                  name='name'
+                  id='name'
+                  placeholder='Enter your name'
+                  value={userInfo.name}
+                  onChange={e => handleChange(e)}
+                  required
+                />
+              </div>
+              <div className={Styles.inputField}>
+                <label htmlFor='room'>Room</label>
+                <input
+                  type='text'
+                  name='room'
+                  id='room'
+                  placeholder='Enter room'
+                  value={userInfo.room}
+                  onChange={e => handleChange(e)}
+                  required
+                />
+              </div>
+              <Link to='/chat' onClick={e => handleSubmit(e)}>
+                <button className={Styles.joinRoomBtn}>Join Room</button>
+              </Link>
+            </div>
           </div>
-          <div className={Styles.inputField}>
-            <label htmlFor='room'>Room</label>
-            <input
-              type='text'
-              name='room'
-              id='room'
-              placeholder='Enter room'
-              value={userInfo.room}
-              onChange={e => handleChange(e)}
-              required
-            />
-          </div>
-          <Link to='/chat' onClick={e => handleSubmit(e)}>
-            <button className={Styles.joinRoomBtn}>Join Room</button>
-          </Link>
+        </div>
+      </div>
+      <div className={Styles.activeRoomsContainer}>
+        <div className={Styles.onlineRoomsContainer}>
+          <OnlineRooms />
         </div>
       </div>
     </div>
--- client/src/components/onlineRooms/OnlineRooms.js
+++ client/src/components/onlineRooms/OnlineRooms.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import Styles from './OnlineRooms.module.css';
 const OnlineRooms = () => {
-  return <div></div>;
+  return (
+    <div className={Styles.container}>
+      <div className={Styles.roomHeader}>Online users</div>
+      <div className={Styles.OnlineRoomsContainer}></div>
+    </div>
+  );
 };
 
 export default OnlineRooms;
